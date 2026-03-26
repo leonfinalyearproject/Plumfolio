92a41e267db0157757c257cc3568efaf54d49e4e
leonfinalyearproject <leonfinalyearproject@gmail.com>
2026-03-26 08:25:21 +0200

    Add files via upload

--- src/utils/registerSW.js
+++ src/utils/registerSW.js
@@ -1,22 +1,12 @@
 // src/utils/registerSW.js
-// Register the service worker for PWA support
-
 export function registerServiceWorker() {
   if ('serviceWorker' in navigator) {
     window.addEventListener('load', () => {
+      const swUrl = '/Plumfolio/service-worker.js';
       navigator.serviceWorker
-        .register(process.env.PUBLIC_URL + '/service-worker.js')
-        .then((registration) => {
-          console.log('SW registered:', registration.scope);
-
-          // Check for updates every 30 minutes
-          setInterval(() => {
-            registration.update();
-          }, 30 * 60 * 1000);
-        })
-        .catch((error) => {
-          console.log('SW registration failed:', error);
-        });
+        .register(swUrl, { scope: '/Plumfolio/' })
+        .then((reg) => console.log('SW registered:', reg.scope))
+        .catch((err) => console.log('SW failed:', err));
     });
   }
 }
